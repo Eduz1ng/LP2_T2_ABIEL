636c795f55a8264009aa8ba0b2ce5fac880e2319
Eduz1ng <eduzing09@gmail.com>
2024-10-13 23:26:03 -0500

    Add files via upload

---
+++ LPII_T2_ECHE/LP_T2_ECHE/src/main/java/com/example/demo/LpT2EcheApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpT2EcheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpT2EcheApplication.class, args);
+	}
+
+}
